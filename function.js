4c75a84a3c2050df7742af76bd6a22edcde1b0e3
Vansh Sharma <118446476+itsvanshsharma@users.noreply.github.com>
2023-03-26 15:11:19 +0530

    commit29:ADD29

--- function.js
+++ function.js
@@ -476,3 +476,235 @@ try{
 catch(err){
     console.log(err)
 }
+
+}
+
+function  appendAuto(data,lat,long){
+let icon=(data[0].weather[0].icon)
+let sup=document.createElement("sup");
+sup.innerText="C"
+let h3Temp=document.createElement("h3");
+let temp=(data[0].temp.day-(273.15)).toFixed(2)
+//console.log(temp)
+h3Temp.innerText=`${temp}°`;
+h3Temp.append(sup);
+
+let div_temp=document.createElement("div");
+
+let div_img=document.createElement("img");
+div_img.src=`https://openweathermap.org/img/w/${icon}.png`;
+
+div_temp.append(div_img);
+
+let h2day=document.createElement("h2");
+const dy=new Date(data[0].dt*1000);
+
+const [day, date,month] = [dy.getDay(), dy.getDate(),dy.getMonth()];
+
+let dt=date.toString();
+let dayr;
+let dtr;
+let mnth;
+if(dt.length<2){
+    dtr=`0${dt}`
+}
+else{
+    dtr=`${dt}`
+}
+if(day===0){
+    dayr="Sunday";
+}
+else if(day===1){
+    dayr="Monday";
+}
+else if(day===2){
+    dayr="Tuesday";
+}
+else if(day===3){
+    dayr="Wednesday";
+}
+else if(day===4){
+    dayr="Thursday";
+}
+else if(day===5){
+    dayr="Friday";
+}
+else if(day===6){
+    dayr="Saturday";
+}
+
+
+if(month===0){
+    mnth="Jan";
+}
+else if(month===1){
+    mnth="Feb";
+}
+else if(month===2){
+    mnth="Mar";
+}
+else if(month===3){
+    mnth="Apr";
+}
+else if(month===4){
+    mnth="May";
+}
+else if(month===5){
+    mnth="Jun";
+}
+else if(month===6){
+    mnth="Jul";
+}
+else if(month===7){
+    mnth="Aug";
+}
+else if(month===8){
+    mnth="Sept";
+}
+else if(month===9){
+    mnth="Oct";
+}
+else if(month===10){
+    mnth="Nov";
+}
+else if(month===11){
+    mnth="Dec";
+}
+h2day.innerText=`${dayr} ${dtr} ${mnth}`;
+// console.log(h2day.innerText);
+// console.log(dy)
+// console.log(day);
+// console.log(date);
+// console.log(month);
+
+
+
+let hr1=document.createElement("hr");
+let div_lat=document.createElement("div");
+let lat_p=document.createElement("p");
+lat_p.innerText=`Latitude`;
+let lat_pv=document.createElement("p");
+ lat_pv.innerText=`${lat}`;
+
+ div_lat.append(lat_p,lat_pv);
+
+ let div_lon=document.createElement("div");
+ let lon_p=document.createElement("p");
+ lon_p.innerText=`Longitude`;
+ let lon_pv=document.createElement("p");
+ lon_pv.innerText=`${long}`;
+ let hr2=document.createElement("hr");
+
+ div_lon.append(lon_p,lon_pv);
+ let water_mark=document.createElement("p");
+ water_mark.setAttribute("id","mark");
+ water_mark.innerText=`Developed by Team Arjun`;
+
+ document.querySelector(".popltn").classList.add("visible")
+
+ document.querySelector(".popltn").append(h2day,hr1,div_lat,div_lon,hr2,water_mark)
+
+
+
+let h5Sky=document.createElement("h5");
+let sky=((data[0].weather[0].main).toUpperCase());
+h5Sky.innerText=`${sky}`;
+
+let div_d1=document.createElement("div");
+div_d1.setAttribute("class","disdetl");
+
+let d1_img=document.createElement("div");
+d1_img.setAttribute("class","img");
+
+let d1_img1=document.createElement("img");
+d1_img1.src=`${logo1}`;
+ let pH=document.createElement("p");
+ pH.innerText=`Humidity`;
+
+ d1_img.append(d1_img1,pH);
+
+ let pHV=document.createElement("p");
+ pHV.innerText=`${data[0].humidity}%`;
+
+ div_d1.append(d1_img,pHV);
+
+
+
+ let div_d2=document.createElement("div");
+div_d2.setAttribute("class","disdetl");
+
+let d2_img=document.createElement("div");
+d2_img.setAttribute("class","img");
+
+let d2_img2=document.createElement("img");
+d2_img2.src=`${logo2}`;
+ let pW=document.createElement("p");
+ pW.innerText=`Wind Speed`;
+
+ d2_img.append(d2_img2,pW);
+
+ let pWV=document.createElement("p");
+ pWV.innerText=`${data[0].wind_speed}`;
+
+ div_d2.append(d2_img,pWV);
+
+
+ let div_d3=document.createElement("div");
+div_d3.setAttribute("class","disdetl");
+
+let d3_img=document.createElement("div");
+d3_img.setAttribute("class","img");
+
+let d3_img3=document.createElement("img");
+d3_img3.src=`${logo3}`;
+ let pMn=document.createElement("p");
+ pMn.innerText=`Min Temp`;
+
+ d3_img.append(d3_img3,pMn);
+
+ let pMnV=document.createElement("p");
+ let tmin=(data[0].temp.min-(273.15)).toFixed(2)
+
+ pMnV.innerText=`${tmin}`
+
+ div_d3.append(d3_img,pMnV);
+
+
+ let div_d4=document.createElement("div");
+div_d4.setAttribute("class","disdetl");
+
+let d4_img=document.createElement("div");
+d4_img.setAttribute("class","img");
+
+let d4_img4=document.createElement("img");
+d4_img4.src=`${logo4}`;
+ let pMm=document.createElement("p");
+ pMm.innerText=`Max Temp`;
+
+ d4_img.append(d4_img4,pMm);
+
+ let pMmV=document.createElement("p");
+ let tmax=(data[0].temp.max-(273.15)).toFixed(2)
+
+ pMmV.innerText=`${tmax}`
+
+ div_d4.append(d4_img,pMmV);
+
+
+
+ let div_d5=document.createElement("div");
+div_d5.setAttribute("class","disdetl");
+
+let d5_img=document.createElement("div");
+d5_img.setAttribute("class","img");
+
+let d5_img5=document.createElement("img");
+d5_img5.src=`${logo5}`;
+ let pSs=document.createElement("p");
+ pSs.innerText=`Sunrise`;
+
+ d5_img.append(d5_img5,pSs);
+
+ let pSsV=document.createElement("p");
+ const time=new Date(data[0].sunrise*1000);
+const [hour, minutes] = [time.getHours(), time.getMinutes()];
